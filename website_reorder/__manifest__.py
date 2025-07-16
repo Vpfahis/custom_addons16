0bfa98dde5ffa06e08e5ac8634f3d1dde9bff0a5
vpfahis <vpfahis8@gmail.com>
2025-07-16 10:50:37 +0530

    ADD

---
+++ website_reorder/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Website Reordering",
+    'version': '16.0.1.0.0',
+    'depends': ['base','web','website'],
+    'author': "Fahis",
+    'category': 'My Category',
+    'description': """
+    A module to reorder in website from the users saleorder history
+    """,
+    # data files always loaded at installation
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
